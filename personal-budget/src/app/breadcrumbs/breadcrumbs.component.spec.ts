7dec69be903b6e55a90d6a8a84c2af86865cba04
SamNewton <sam@samnewton.net>
2020-10-08 15:43:42 -0400

    finished instructions

--- personal-budget/src/app/breadcrumbs/breadcrumbs.component.spec.ts
+++ personal-budget/src/app/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { P404Component } from './p404.component';
+import { BreadcrumbsComponent } from './breadcrumbs.component';
 
-describe('P404Component', () => {
-  let component: P404Component;
-  let fixture: ComponentFixture<P404Component>;
+describe('BreadcrumbsComponent', () => {
+  let component: BreadcrumbsComponent;
+  let fixture: ComponentFixture<BreadcrumbsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ P404Component ]
+      declarations: [ BreadcrumbsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(P404Component);
+    fixture = TestBed.createComponent(BreadcrumbsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- personal-budget/src/app/contact/contact.component.spec.ts
+++ personal-budget/src/app/contact/contact.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { P404Component } from './p404.component';
+import { ContactComponent } from './contact.component';
 
-describe('P404Component', () => {
-  let component: P404Component;
-  let fixture: ComponentFixture<P404Component>;
+describe('ContactComponent', () => {
+  let component: ContactComponent;
+  let fixture: ComponentFixture<ContactComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ P404Component ]
+      declarations: [ ContactComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(P404Component);
+    fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
